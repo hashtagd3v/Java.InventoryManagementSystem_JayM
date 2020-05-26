5f9b55fa7a551c691653b88359969906cddede52
Jay M <jaycoinminer@gmail.com>
2020-05-25 20:33:34 -0500

    Done :  INVENTORY MUST BE BETWEEN MIN AND MAX VALUE FOR PART/PRODUCT. - DONE. Done : PRICE OF PRODUCT CANNOT BE LESS THAN COST OF ALL ASSOCIATED PARTS.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -23,7 +23,8 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        //Parts:
+        // Declare Test Parts:
+
         InHousePart part1 = new InHousePart(0, "Part A", 3.99, 5, 1, 100, 3133);
         OutSourcedPart part2 = new OutSourcedPart(0, "Part B", 2.00, 3, 1, 50, "MicroCenter");
         OutSourcedPart part3 = new OutSourcedPart(0, "Part C", 29.99, 10, 5, 30, "Samsung");
@@ -32,7 +33,8 @@ public class Main extends Application {
         Inventory.addPart(part2);
         Inventory.addPart(part3);
 
-        //Products:
+        //Declare Test Products:
+
         Product product1 = new Product(0, "Product A", 399.99, 10, 5, 25);
         Product product2 = new Product(0, "Product B", 299.99, 5, 5, 30);
         Product product3 = new Product(0, "Product C", 199.99, 8, 3, 15);
